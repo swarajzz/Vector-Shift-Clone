bf1b619db2d4e15f6534c3dde380cd0a44d85c1c
Swaraj <sharma.swaraj0403@gmail.com>
2024-10-14 00:47:58 +0530

    check for text area

--- frontend/src/NodeBase.js
+++ frontend/src/NodeBase.js
@@ -1,8 +1,8 @@
 import { Handle, Position } from "reactflow";
 import { useState } from "react";
+import { CircleX } from "lucide-react";
 
 export const NodeBase = ({ id, data, type, icon: Icon, fields, handles }) => {
-  console.log("HEy");
   const [state, setState] = useState(data);
   const handleChange = (field, value) => {
     setState((prevState) => ({
@@ -12,43 +12,62 @@ export const NodeBase = ({ id, data, type, icon: Icon, fields, handles }) => {
   };
 
   return (
-    // <div className="p-4 w-[200px] h-[100px] border border-gray-300 bg-white shadow-md rounded-md">
-    <div className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-400">
-      {/* <div className="mb-2 text-sm font-semibold text-gray-700"><Icon /></div> */}
-      <div className="flex items-center mb-2 text-sm font-semibold text-gray-700">
-        {Icon && <Icon width={18} height={18} />}
-        <span>{type}</span>
+    <div className="border border-gray-400 group rounded-md bg-white py-2 shadow-purple transition-all hover:border-[#a9a8f7] hover:shadow-purple-2">
+      <div className="mb-2 flex items-center justify-between px-2 text-sm font-semibold text-gray-700">
+        <div className="flex items-center justify-center gap-2 text-[#5d7f9e] group-hover:text-[#6563e4]">
+          {Icon && <Icon width={18} height={18} />}
+          <span>{type}</span>
+        </div>
+        <div>
+          <CircleX
+            width={16}
+            height={18}
+            className="mx-0 cursor-pointer transition hover:text-red-500"
+          />
+        </div>
       </div>
-      <div className="space-y-2">
+
+      <div className="space-y-2 px-4">
         {fields.map((field) => (
-          <label
-            key={field.name}
-            className="flex flex-col text-sm text-gray-600"
-          >
-            {field.label}:
-            {field.type === "text" ? (
-              <input
-                className="mt-1 p-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                type="text"
-                value={state[field.name]}
-                onChange={(e) => handleChange(field.name, e.target.value)}
-              />
-            ) : field.type === "select" ? (
-              <select
-                className="mt-1 p-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                value={state[field.name]}
-                onChange={(e) => handleChange(field.name, e.target.value)}
-              >
-                {field.options.map((option) => (
-                  <option key={option} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
-            ) : null}
-          </label>
+          <div key={field.name}>
+            {field.textArea ? (
+              <>
+                <span>{field.label}</span>
+                <textarea
+                  className="focus:shadow-[0_0_0_2px #6366F1] mt-1 overflow-auto overflow-y-auto overflow-x-hidden rounded-md border border-[#d9d8dd] p-1 text-[#313745] transition hover:border-[#f1f1fe] hover:bg-[#fafaff] focus:bg-transparent focus:outline-none group-hover:text-[#6563e4] block resize w-full cursor-text"
+                  onInput={(e) =>
+                    handleChange(field.name, e.currentTarget.textContent)
+                  }
+                ></textarea>
+              </>
+            ) : (
+              <>
+                <div>{field.label}</div>
+
+                {field.type === "text" ? (
+                  <input
+                    className="focus:shadow-[0_0_0_2px #6366F1] p-1 rounded-md border border-[#d9d8dd] text-[#313745] transition hover:border-[#f1f1fe] hover:bg-[#fafaff] focus:bg-transparent focus:outline-none group-hover:text-[#6563e4] min-w-44"
+                    type="text"
+                    onChange={(e) => handleChange(field.name, e.target.value)}
+                  />
+                ) : field.type === "select" ? (
+                  <select
+                    className="mt-1 rounded-md border border-gray-300 p-1 focus:outline-none group-hover:text-[#6563e4]"
+                    onChange={(e) => handleChange(field.name, e.target.value)}
+                  >
+                    {field.options.map((option) => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </select>
+                ) : null}
+              </>
+            )}
+          </div>
         ))}
       </div>
+
       {handles.map((handle) => (
         <Handle
           key={handle.id}
@@ -56,7 +75,7 @@ export const NodeBase = ({ id, data, type, icon: Icon, fields, handles }) => {
           position={handle.position}
           id={`${id}-${handle.id}`}
           style={handle.style}
-          className="w-2 h-2 bg-blue-500"
+          className="h-2 w-2 bg-blue-500"
         />
       ))}
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'purple': "#cecffc 0px 0px 0px 2px",
+        'purple-2': "#cecffc 0px 0px 0px 4px"
+      }
+    },
   },
   plugins: [],
 };
